fdbb66d453411a5f917bf88040a940b245b4e095
juror.yu <juror.yu@mart.blue>
2019-05-28 09:33:19 +0800

    待验证tx

---
+++ src/main/java/com/zhyyu/learn/spring/transaction/TODO.java
@@ -0,0 +1,14 @@
+package com.zhyyu.learn.spring.transaction;
+
+/**
+ * 待验证项目
+ * <pre>
+ *     1. targetObject 中调用 @Transactional 方法是否触发事务
+ *     2. propagation require_new 外层/内层 事务回滚提交
+ *     3. propagation nested 外层/内层 事务回滚提交
+ * </pre>
+ * @author juror
+ * @datatime 2019/5/28 9:30
+ */
+public class TODO {
+}
