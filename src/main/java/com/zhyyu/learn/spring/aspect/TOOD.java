c327a8fa2d11132e410b3b25faba1ce149630ce0
juror.yu <juror.yu@mart.blue>
2019-05-31 09:55:04 +0800

    aop 待测试: 接口注解, 实现类注解

---
+++ src/main/java/com/zhyyu/learn/spring/aspect/TOOD.java
@@ -0,0 +1,15 @@
+package com.zhyyu.learn.spring.aspect;
+
+/**
+ * 待测试
+ * <pre>
+ *     1. 类实现给定接口, 接口方法增加注解, 添加该注解相关切面
+ *      - 不指定proxy 实现(默认动态代理?, 非cglib
+ *      - 指定proxy 实现(cglib
+ *      - 观察advice 是否会执行
+ * </pre>
+ * @author juror
+ * @datatime 2019/5/31 9:50
+ */
+public class TOOD {
+}
