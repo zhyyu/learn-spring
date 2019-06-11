e73df9790b5f1d5bcf53bdd684ae9d5fde135d58
juror.yu <juror.yu@mart.blue>
2019-06-11 19:32:08 +0800

    增加mvc, 与ioc 待测试项目

---
+++ src/main/java/com/zhyyu/learn/spring/ioc/TODO.java
@@ -0,0 +1,12 @@
+package com.zhyyu.learn.spring.ioc;
+
+/**
+ * 待测试项
+ * <pre>
+ *     1. @PostConstruct 是否在所有Bean 全部装配好后执行
+ * </pre>
+ * @author juror
+ * @datatime 2019/6/11 19:28
+ */
+public class TODO {
+}
---
+++ src/main/java/com/zhyyu/learn/spring/mvc/TODO.java
@@ -0,0 +1,18 @@
+package com.zhyyu.learn.spring.mvc;
+
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ * <p>
+ *     1. spring mvc 拦截方式: 1-filter 2-handlerInterceptor 3-aop
+ *     2. spring mvc 中handler 为处理器抽象, 可为controller, 也可为struct2? action, 需要handler adapter 做包装, 统一接口
+ * </p>
+ * @author juror
+ * @datatime 2019/6/3 16:09
+ */
+public class TODO {
+
+    public static void main(String[] args) {
+    }
+
+}
